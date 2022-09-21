583da6172046c91d1682dc63bee94b5cb5301ca9
0538won <tmddnjschfhr@gmail.com>
2022-09-21 17:14:11 +0900

    Update hello_world.py

--- 실습자료/hello_world.py
+++ 실습자료/hello_world.py
@@ -1,5 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def printf_KIT():
+    printf("Kumoh National Institute of Technology.")
+    
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
